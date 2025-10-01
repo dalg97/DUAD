9a8587ab3b9ac299c77297c883492137add83cae
Diego Lopez <diegologa22.dl@gmail.com>
2025-09-30 19:50:31 -0600

    Added the menu

---
+++ Semana10/main.py
@@ -0,0 +1,9 @@
+def show_menu():
+    print(f"1. Add a new student\n"
+    f"2. Export students in CSV\n"
+    f"3. Import new students\n"
+    f"4. Exit")
+    option = int(input("Enter an option: "))
+
+
+show_menu 
