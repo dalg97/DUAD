e9ee1c8b8fe3b2632245bbe65e43b6624dd219ce
Diego Lopez <diegologa22.dl@gmail.com>
2026-04-29 20:46:39 -0600

    Exercise 1 modified

--- Python Intermedio/Ejercicios_Decoradores/Ejercicio1.py
+++ Python Intermedio/Ejercicios_Decoradores/Ejercicio1.py
@@ -2,11 +2,12 @@ def decorator1(func):
     def wrapper(a,b):
         print(f"{a} and {b} are the parameters in the decorator")
         result = func(a,b)
+        print(f"Result: {result}")
         return result
     return wrapper
 
 @decorator1
 def print_parameters(a,b):
-    print(f"{a} and {b} are the parameters inside the function")
+    return f"The order of the parameter has changed: {b} and {a}"
 
 print_parameters(5,2)
